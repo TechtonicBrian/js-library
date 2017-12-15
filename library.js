800ac463fc515507233c4834c4bb5e6bc65d0a10
LAPTOP-CKRVAV6F\student13 <bnelson@techtonicgroup.com>
2017-12-15 14:50:49 -0700

    Update as I go.

--- library.js
+++ library.js
@@ -11,6 +11,7 @@ var Book = function(oArgs) {
   this.publishDate = new Date(oArgs.publishDate);
 };
 
+//Initial function that runs on Document ready
 Library.prototype.init = function() {
   this._bindEvents();
 
@@ -20,35 +21,55 @@ Library.prototype.init = function() {
     $("#adminMgmt").toggle(500);
   });
 
-  // Add more books
-  $(".addAnother").on("click", function() {
-    $("#appMore:last").clone().appendTo($("#addMore"));
+  // Add more books form incrementer and button management
+  $("#addBooksButton").hide();
+  $("#oneMoreBookButton").on("click", function() {
+    $("#oneMoreBookForm:last").clone().appendTo($("#addBooksRow"));
+    $("#addBooksButton").show();
+    $("#addBookButton").hide();
   });
 
-  // Remove title or author
+  // Remove title or author selection
   $(function() {
-    var $select = $(".removeWhat"),
+    var $select = $("#removeBySelector"),
         $forms = $(".toggleRemove");
     $select.on("change", function() {
       var value = "." + $(this).val();
       $forms.show().not(value).hide();
     });
   });
+
+  // Basic search button management - NEEDS ATTENTION
+  $("#searchByTitleButton").hide();
+  $("#searchByAuthorButton").hide();
+  $("#").on("click", function() {
+    $("#")
+  });
 };
 
+//Event handlers that bind events to functions
 Library.prototype._bindEvents = function() {
   $("#addBookButton").on("click", $.proxy(this._handleAddBook, this));
+  $("#addBooksButton").on("click", $.proxy(this._handleAddBooks, this));
+  $("#removeByTitleButton").on("click", $.proxy(this._handleRemoveBooksByTitle, this));
+  $("#removeByAuthorButton").on("click", $.proxy(this._handleRemoveBooksByAuthor, this));
+  //NEED HANDLER FOR GET ALL BOOKS?
+  $("#getRandomBookButton").on("click", $.proxy(this._handleGetRandomBook, this));
+  $("#getAllAuthorsButton").on("click", $.proxy(this._handleGetAuthors, this));
+  $("#getRandomAuthorNameButton").on("click", $.proxy(this._handleGetRandomAuthorName, this));
+  $("#searchByKeywordButton").on("click", $.proxy(this._handleGetBooksByKeyword, this));
+  $("#searchByTitleButton").on("click", $.proxy(this._handleGetBooksByTitle, this));
+  $("#searchByAuthorButton").on("click", $.proxy(this._handleGetBooksByAuthor, this));
 };
 
 //AddBook functionality
 Library.prototype.addBook = function(book) {
-  for (var i = 0; i < this.books.length; i++) { //loop/iterate through all indeces of books
-    // debugger;
-    if (this.books[i].title === book.title) { //check if title of incoming book matches title of current index in books
+  for (var i = 0; i < this.books.length; i++) {
+    if (this.books[i].title === book.title) {
       return false;
     }
   }
-  this.books.push(book); //otherwise, add book object
+  this.books.push(book);
   return true;
 };
 
@@ -61,95 +82,100 @@ Library.prototype._handleAddBook = function(oArgs) {
   this.addBook(newBook);
 };
 
-
-//RemoveBooksByTitle functionality
-Library.prototype.removeBooksByTitle = function(title) {
-  for (var i = 0; i < this.books.length; i++) { //loop/iterate through all indeces of books
-    if (this.books[i].title === title) { //check if string matches title of current index in books
-        this.books.splice(i, 1); //delete index of array
-        return true;
-    }
-  }
-  return false;
-};
-
-//RemoveBooksByAuthor functionality
-Library.prototype.removeBooksByAuthor = function(authorName) {
-  var temp = []; //create a new empty array in scope of function
-  for (var i = 0; i < this.books.length; i++) {
-    if (this.books[i].author !== authorName) { //check if string does not match authorName of current index in books
-      temp.push(this.books[i]); //return book object(s) as array
-    }
-  }
-  if (temp.length < this.books.length) { //if new array is smaller than original array
-    this.books = temp; //assign value of orig array to new array
-    return true;
-  }
-  return false; //otherwise, return false
-};
-
-//GetRandomBook functionality
-Library.prototype.getRandomBook = function() {
-  if (this.books.length === 0) { //check if array is empty
-    return null;
-  }
-    return this.books[Math.floor(Math.random()*this.books.length)]; //otherwise, return random book object
-    //document.getElementById("bookResults").innerHTML =
-};
-
-//GetBooksByTitle functionality
-Library.prototype.getBooksByTitle = function(title) {
-  var temp = []; //create a new empty array in scope of function
-  for (var i = 0; i < this.books.length; i++) { //loop/iterate through all indeces of books
-    if (this.books[i].title.toLowerCase().indexOf(title.toLowerCase()) !== -1) { //check if titles in books contain parameter
-      temp.push(this.books[i]); //return book object(s) as array
-    }
-  }
-  return temp; //otherwise, return new empty array
-};
-
-//GetBooksByAuthor functionality
-Library.prototype.getBooksByAuthor = function(authorName) {
-  var temp = []; //create new empty array in scope of function
-  for (var i = 0; i < this.books.length; i++) { //loop/iterate through all indeces of books
-    if (this.books[i].author.toLowerCase().indexOf(authorName.toLowerCase()) !== -1) { //check if names in books contain parameter
-      temp.push(this.books[i]); //return book object(s)
-    }
-  }
-  return temp; //otherwise, return new empty array
-};
-
 //AddBooks functionality
 Library.prototype.addBooks = function(books) {
   var bookCount = 0;
-  for (var i = 0; i < books.length; i++) { //loop/iterate through all indeces of books parameter
-    if (this.addBook(books[i])) { //check if index element needs addBook function
+  for (var i = 0; i < books.length; i++) {
+    if (this.addBook(books[i])) {
       bookCount++;
     }
   }
   return bookCount;
 };
 
-//GetAuthors functionality
-Library.prototype.getAuthors = function() {
-  var temp = []; //create new empty array in scope of function
-  for (var i = 0; i < this.books.length; i++) { //loop/iterate through all indeces of books
-    if (temp.indexOf(this.books[i].author) === -1) { //
-      temp.push(this.books[i].author); //return authorName values
+//NEEDS ATTENTION
+Library.prototype._handleAddBooks = function(oArgs) {
+  for (var i = 0; i < oArgs.length; i++) {
+    this.addBook(newBook);
+  }
+};
+
+//RemoveBooksByTitle functionality
+Library.prototype.removeBooksByTitle = function(title) {
+  for (var i = 0; i < this.books.length; i++) {
+    if (this.books[i].title === title) {
+        this.books.splice(i, 1);
+        return true;
     }
   }
-  return temp; //otherwise, return new empty array
+  return false;
+};
+
+Library.prototype._handleRemoveBooksByTitle = function() {
+
+};
+
+//RemoveBooksByAuthor functionality
+Library.prototype.removeBooksByAuthor = function(authorName) {
+  var temp = [];
+  for (var i = 0; i < this.books.length; i++) {
+    if (this.books[i].author !== authorName) {
+      temp.push(this.books[i]);
+    }
+  }
+  if (temp.length < this.books.length) {
+    this.books = temp;
+    return true;
+  }
+  return false;
+};
+
+Library.prototype._handleRemoveBooksByAuthor = function() {
+
+};
+
+//BROWSE FUNCTIONALITY??? - SHOW FULL LIBRARY
+
+//GetRandomBook functionality
+Library.prototype.getRandomBook = function() {
+  if (this.books.length === 0) {
+    return null;
+  }
+    return this.books[Math.floor(Math.random()*this.books.length)];
+};
+
+Library.prototype._handleGetRandomBook = function() {
+
+};
+
+//GetAuthors functionality
+Library.prototype.getAuthors = function() {
+  var temp = [];
+  for (var i = 0; i < this.books.length; i++) {
+    if (temp.indexOf(this.books[i].author) === -1) {
+      temp.push(this.books[i].author);
+    }
+  }
+  return temp;
+};
+
+Library.prototype._handleGetAuthors = function() {
+
 };
 
 //GetRandomAuthorName functionality
 Library.prototype.getRandomAuthorName = function() {
-  if (this.books.length === 0) { //check if array is empty
+  if (this.books.length === 0) {
     return null;
   }
-    return this.books[Math.floor(Math.random()*this.books.length)].author; //otherwise, return random authorName string
+    return this.books[Math.floor(Math.random()*this.books.length)].author;
 };
 
-// Robust Search functionality (by Keyword) - NEEDS ATTENTION
+Library.prototype._handleGetRandomAuthorName = function() {
+
+};
+
+// Robust Search functionality (by Keyword)
 Library.prototype.getBooksByKeyword = function(keyword) {
   var temp = [];
   for (var i = 0; i < this.books.length; i++) {
@@ -160,6 +186,41 @@ Library.prototype.getBooksByKeyword = function(keyword) {
   return temp;
 };
 
+Library.prototype._handleGetBooksByKeyword = function() {
+
+};
+
+//GetBooksByTitle functionality
+Library.prototype.getBooksByTitle = function(title) {
+  var temp = [];
+  for (var i = 0; i < this.books.length; i++) {
+    if (this.books[i].title.toLowerCase().indexOf(title.toLowerCase()) !== -1) {
+      temp.push(this.books[i]);
+    }
+  }
+  return temp;
+};
+
+Library.prototype._handleGetBooksByTitle = function() {
+
+};
+
+//GetBooksByAuthor functionality
+Library.prototype.getBooksByAuthor = function(authorName) {
+  var temp = [];
+  for (var i = 0; i < this.books.length; i++) {
+    if (this.books[i].author.toLowerCase().indexOf(authorName.toLowerCase()) !== -1) {
+      temp.push(this.books[i]);
+    }
+  }
+  return temp;
+};
+
+Library.prototype._handleGetBooksByAuthor = function() {
+
+};
+
+//Array of books
 var booksArray = [
   new Book({title:"Coders at Work: Reflections on the Craft of Programming", author:"Peter Seibel", numberOfPages: 1303, publishDate: "01/01/1999"}),
   new Book({title:"Code Complete: A Practical Handbook of Software Construction", author:"Steve McConnell", numberOfPages: 336, publishDate: "01/01/1999"}),
@@ -173,9 +234,12 @@ var gBookThree = new Book({title:"Front-end web development: the Big Nerd Ranch 
 var gBookFour = new Book({title:"Web Development Foundations: Full-Stack vs Front-End", author:"Ray Villalobos", numberOfPages: 1010, publishDate: "01/01/1999"});
 var gBookFive = new Book({title:"Getting a coding job for dummies", author:"Nikhil Abraham", numberOfPages: 297, publishDate: "01/01/1999"});
 
+//Display functions
 
 
 
+
+//Document ready
 $(function(){
   window.library = new Library()
   window.library.init()
